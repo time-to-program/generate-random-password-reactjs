8670afeb84d7e331c281ec7e33884475d631ebcf
raviteja-v <timeforprogramming@gmail.com>
2023-07-01 19:53:30 +0530

    Generating ramdom password with config

--- src/App.js
+++ src/App.js
@@ -1,23 +1,152 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [options, setOptions] = useState({
+    length: 10,
+    uppercase: false,
+    lowercase: false,
+    number: false,
+    symbols: false,
+    isError: false,
+  });
+
+  const [isError, setIsError] = useState(false);
+  const [generatedPassword, setGeneratedPassword] = useState("");
+
+  const generateRandomPassword = () => {
+    if (
+      !options?.uppercase &&
+      !options?.lowercase &&
+      !options?.number &&
+      !options?.symbols
+    ) {
+      setIsError(true);
+      return;
+    }
+
+    setIsError(false);
+
+    const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+    const numberChars = "0123456789";
+    const symbolChars = '!@#$%^&*()_+-={}[]|:;"<>,.?/~';
+
+    let passwordChars = "";
+    let password = "";
+
+    if (options.uppercase) {
+      passwordChars += uppercaseChars;
+    }
+
+    if (options.lowercase) {
+      passwordChars += lowercaseChars;
+    }
+
+    if (options.number) {
+      passwordChars += numberChars;
+    }
+
+    if (options.symbols) {
+      passwordChars += symbolChars;
+    }
+
+    const passwordLength = options.length;
+
+    for (let i = 0; i < passwordLength; i++) {
+      const randomIndex = Math.floor(Math.random() * passwordChars.length);
+      password += passwordChars[randomIndex];
+    }
+
+    setGeneratedPassword(password);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="card">
+        <div className="card-header">
+          <p className="title">Generate Random Password</p>
+        </div>
+
+        <div className="card-body">
+          <label>Password length</label>
+          <input
+            value={options.length}
+            onChange={({ target }) => {
+              setOptions({ ...options, length: target.value });
+            }}
+            name="confirmPassword"
+            type="number"
+            placeholder="Password length"
+            min={4}
+          />
+
+          <div className="row">
+            <div className="checkbox-container">
+              <input
+                type="checkbox"
+                name="languages"
+                checked={options.uppercase}
+                onChange={() => {
+                  setOptions({ ...options, uppercase: !options.uppercase });
+                }}
+              />
+              <label>Uppercase</label>
+            </div>
+            <div className="checkbox-container">
+              <input
+                type="checkbox"
+                name="languages"
+                checked={options.lowercase}
+                onChange={() => {
+                  setOptions({ ...options, lowercase: !options.lowercase });
+                }}
+              />
+              <label>Lowercase</label>
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="checkbox-container">
+              <input
+                type="checkbox"
+                name="languages"
+                checked={options.number}
+                onChange={() => {
+                  setOptions({ ...options, number: !options.number });
+                }}
+              />
+              <label>Number</label>
+            </div>
+
+            <div className="checkbox-container">
+              <input
+                type="checkbox"
+                name="languages"
+                checked={options.symbols}
+                onChange={() => {
+                  setOptions({ ...options, symbols: !options.symbols });
+                }}
+              />
+              <label>Symbols</label>
+            </div>
+          </div>
+
+          {isError && (
+            <span className="error">Please selete atleast one option</span>
+          )}
+
+          <button className="btn" onClick={generateRandomPassword}>
+            Generate Password
+          </button>
+        </div>
+      </div>
+
+      {generatedPassword && (
+        <div className="password">
+          <label>Generated Password:</label>
+          <p>{generatedPassword}</p>
+        </div>
+      )}
     </div>
   );
 }
